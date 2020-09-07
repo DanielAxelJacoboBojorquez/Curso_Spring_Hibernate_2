a04b02f05eba0d6afc1c92ffb57c4e26182ba096
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-09-06 19:31:09 -0700

    Relations between Tables - Delete Customer Details

---
+++ src/es/pildoras/conexionHibernate/EliminaDetallesCliente.java
@@ -0,0 +1,38 @@
+package es.pildoras.conexionHibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class EliminaDetallesCliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1.- Crear SessionFactory
+		SessionFactory miFactory=new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Cliente.class)
+				.addAnnotatedClass(DetallesCliente.class)
+				.buildSessionFactory();
+		
+		// 2.- Crear obj Session
+		Session miSession=miFactory.openSession();
+		
+		// 3.- Crear obj (Clientes)
+		try {
+			miSession.beginTransaction();
+			DetallesCliente detallesDelCliente=miSession.get(DetallesCliente.class, 4);
+			if(detallesDelCliente!=null) {
+				//System.out.println("Voy a eliminar al cliente "+elCliente.getNombre());
+				miSession.delete(detallesDelCliente);
+			}
+			miSession.getTransaction().commit();
+			if(detallesDelCliente!=null) System.out.println("Registro eliminado correctamente en BBDD");
+			else System.out.println("Nada que eliminar!");
+			miSession.close();
+		}finally {
+			miFactory.close();
+		}
+	}
+
+}
